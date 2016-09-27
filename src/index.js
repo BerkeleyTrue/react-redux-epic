cdcee934c89854c9962d256d7ab9d5bf9bf486a7
Berkeley Martinez <Berkeley@robotie.com>
2016-09-27 01:00:22 -0700

    Feat: export wrapRootEpic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as contain } from './contain';
 export { default as renderToString } from './render-to-string';
 export { default as render } from './render';
+export { default as wrapRootEpic } from './wrap-root-epic';
