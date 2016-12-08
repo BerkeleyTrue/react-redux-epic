6dd91fe87013afaaafe4db09236550f195334bfc
Berkeley Martinez <Berkeley@robotie.com>
2016-12-08 03:39:42 -0800

    fix: remove contain module This will be a separate package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default as contain } from './contain';
 export { default as renderToString } from './render-to-string';
 export { default as render } from './render';
 export { default as wrapRootEpic } from './wrap-root-epic';
