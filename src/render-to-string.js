adaf4ed86b7b4a25f5e103555b3b5cb9475fd53b
Berkeley Martinez <Berkeley@robotie.com>
2017-03-11 18:02:24 -0800

    feat(rts): Wrap initial render pass in obs defer
    We wrap the initial render pass in a observable. The prevents the initial render pass from happening
    unless the observable returned from rts is actually subscribed.

--- src/render-to-string.js
+++ src/render-to-string.js
@@ -1,8 +1,9 @@
-import { Observable } from 'rxjs/Observable';
-import { last } from 'rxjs/operator/last';
-import { map } from 'rxjs/operator/map';
 import ReactDOM from 'react-dom/server';
 import debug from 'debug';
+import { Observable } from 'rxjs/Observable';
+import { defer } from 'rxjs/observable/defer';
+import { last } from 'rxjs/operator/last';
+import { map } from 'rxjs/operator/map';
 
 const log = debug('react-redux-epic:render-to-string');
 
@@ -10,21 +11,23 @@ const log = debug('react-redux-epic:render-to-string');
 //   Component: ReactComponent,
 //   epicMiddleware: EpicMiddleware
 // ) => Observable[String]
-
 export default function renderToString(element, wrappedEpic) {
-  try {
-    log('initial render pass started');
-    ReactDOM.renderToStaticMarkup(element);
-    log('initial render pass completed');
-  } catch (e) {
-    return Observable.throw(e);
+  const observableEpic = Observable.create(wrappedEpic.subscribe);
+  function initialRender() {
+    try {
+      log('first app render');
+      ReactDOM.renderToStaticMarkup(element);
+    } catch (e) {
+      return Observable.throw(e);
+    }
+    wrappedEpic.end();
+    return observableEpic;
   }
-  log('calling action$ onCompleted');
-  wrappedEpic.end();
-  return Observable.create(wrappedEpic.subscribe)
+  return defer(initialRender)
     ::last(null, null, null)
     ::map(() => {
       wrappedEpic.restart();
+      log('final app render');
       const markup = ReactDOM.renderToString(element);
       return { markup };
     });
--- src/wrap-root-epic.js
+++ src/wrap-root-epic.js
@@ -3,49 +3,59 @@ import { EmptyObservable } from 'rxjs/observable/EmptyObservable';
 import { Subject } from 'rxjs/Subject';
 import { Subscriber } from 'rxjs/Subscriber';
 import { EPIC_END } from 'redux-observable/lib/EPIC_END';
+import debug from 'debug';
 
 const endAction = { type: EPIC_END };
+const log = debug('react-redux-epic:wrapped-epic');
 
-export default function wrapRootEpic(rootEpic) {
+export default function wrapRootEpic(userEpic) {
   let actionsProxy = EmptyObservable.create();
   let lifecycle = EmptyObservable.create();
   let subscription;
   let start;
-  function wrappedEpic(actions, ...rest) {
+  let name = `observable(${userEpic.name || 'rootEpic'})`;
+  function observableEpic(actions, ...rest) {
     const results = new Subject();
     start = () => {
       subscription = new Subscriber();
       actionsProxy = new Subject();
-      // how can subject inherit from ActionsObservable
+      // how can we make Subject inherit from the ActionsObservable?
       actionsProxy.ofType = actions.ofType;
       lifecycle = new Subject();
       const actionsSubscription = actions.subscribe(actionsProxy);
-      const epicsSubscription = rootEpic(actionsProxy, ...rest)
+      const epicsSubscription = userEpic(actionsProxy, ...rest)
         .subscribe(
           action => results.next(action),
-          err => { throw err; },
-          () => lifecycle.complete()
+          err => results.error(err),
+          () => {
+            lifecycle.complete();
+            results.complete();
+          }
         );
 
       subscription.add(epicsSubscription);
       subscription.add(actionsSubscription);
     };
+    log(`starting ${name}`);
     start();
     return results;
   }
 
-  wrappedEpic.subscribe =
-    (...args) => lifecycle.subscribe.apply(lifecycle, args);
-  wrappedEpic.unsubscribe = () => subscription.unsubscribe();
-  wrappedEpic.end = () => {
+  observableEpic.displayName = name;
+
+  observableEpic.subscribe =
+    (...args) => lifecycle.subscribe(...args);
+  observableEpic.unsubscribe = () => subscription.unsubscribe();
+  observableEpic.end = () => {
     actionsProxy.next(endAction);
     actionsProxy.complete();
   };
-  wrappedEpic.restart = () => {
-    wrappedEpic.unsubscribe();
+  observableEpic.restart = () => {
+    log(`restarting ${name}`);
+    observableEpic.unsubscribe();
     actionsProxy.unsubscribe();
     start();
   };
 
-  return wrappedEpic;
+  return observableEpic;
 }
